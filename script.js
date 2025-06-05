fa650844d0df885c7d292eee2193d8eed3c906b3
shineincode <easter.nyarko@hotmail.com>
2025-06-05 22:37:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+// Show a welcome message when the page loads
+window.onload = function () {
+  alert("Welcome to the Mom Routine App! You’re doing great!");
+};
 
+// Respond to button clicks
+function greetUser() {
+  alert("You’ve got this, Supermom!");
+}
